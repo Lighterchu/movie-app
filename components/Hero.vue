6cce2df2b8e26411d839fff69622d6ec95a98a9f
Lighterchu <benjaminrowe0@gmail.com>
2024-08-14 16:32:23 +1000

    have added sign in and login button. Plus I have added search bar

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,16 +1,35 @@
 <template>
-    <div class="header bg-gray-900">
-        <div class="nav-container text-red-600 p-2 font-medium">
-            <span>
-                <h1>Quick Move</h1>
-            </span>
-        </div>
+  <div class="header bg-gray-900">
+    <div class="nav-container text-red-600 p-2 font-medium flex">
+      <span>
+        <h1>Quick Move</h1>
+      </span>
+      <div class="m-auto">
+        <label class="mr-2" for="">Search Movie</label>
+        <input class="rounded-md" type="text" name="" id="">
+      </div>
+      <div class=" right-0 ml-auto">
+        <ul class="flex space-x-2">
+            <NuxtLink
+                class="  p-1 sm:bg-red-700  lg:bg-green-600 rounded-md hover:bg-green-500"
+                to="https:www.google.com"
+                >Login
+            </NuxtLink>
+            <NuxtLink
+                class="  p-1 sm:bg-red-700  lg:bg-green-600 rounded-md hover:bg-green-500"
+                to="https:www.google.com"
+                >Register
+            </NuxtLink>
+          <!-- <li>Login</li>
+          <li>Register</li> -->
+        </ul>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Hero',
+  name: "Hero",
 };
 </script>
-
