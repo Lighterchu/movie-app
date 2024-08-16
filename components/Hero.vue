5ca45916f4464a33fca5fabf15f6aac93334fc7f
Lighterchu <benjaminrowe0@gmail.com>
2024-08-16 19:14:00 +1000

    have added a better way for login in and register accounts

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,66 +1,77 @@
-    <template>
-    <div class="header bg-gray-900 mb-10">
-        <div class="nav-container text-red-600 p-2 font-medium flex">
-        <!-- Mobile View -->
-        <div
-            class="bg-gray-900 top-0 sm:block lg:hidden w-full h-20 left-0 absolute flex justify-center"
-        >
-            <!-- <NuxtLink class="p-1 lg:bg-black rounded-md hover:bg-white" to="/"
+<template>
+  <div class="header bg-gray-900 mb-10">
+    <div class="nav-container text-red-600 p-2 font-medium flex">
+      <!-- Mobile View -->
+      <div
+        class="bg-gray-900 top-0 sm:block lg:hidden w-full h-20 left-0 absolute flex justify-center"
+      >
+        <!-- <NuxtLink class="p-1 lg:bg-black rounded-md hover:bg-white" to="/"
             >Quick Move
             </NuxtLink> -->
-            <div class="flex p-1 ">
-                <nuxt-link to="/">
-                    <NuxtImg src="/logo.png" sizes="sm:60px md:60px" class="rounded-full" />
-                </nuxt-link>
-                <label class="mr-2 mt-2 ml-5">Search Movie</label>
-            <input
-            class="w-1/4 h-2/4 text-black mt-2 rounded-md pl-2 mr-2"
+        <div class="flex p-1">
+          <nuxt-link to="/">
+            <NuxtImg
+              src="/logo.png"
+              sizes="sm:60px md:60px"
+              class="rounded-full"
+            />
+          </nuxt-link>
+          <label class="mr-2 mt-2 ml-5">Search Movie</label>
+          <input
+            class="w-20 h-2/4 text-black mt-2 rounded-md pl-2 "
             type="text"
             name=""
             id=""
-            />
-            <button class="hover:bg-white rounded-md sm:right-10 right-1 absolute mt-2 mr-1 p-1 bg-black">Search</button>
-            </div>
+          />
+          <button
+            class="hover:bg-white rounded-md sm:right-16  right-1 absolute  mr-1 p-1 bg-black"
+          >
+            Search
+          </button>
+          <NuxtLink class="p-1 absolute right-0 bg-black mr-2 rounded-md hover:bg-white" to="login"
+            >Sign In
+          </NuxtLink>
         </div>
-        <!-- --- -->
+      </div>
+      <!-- --- -->
 
-        <NuxtLink
-            class="p-1 lg:bg-black md:bg-white rounded-md hover:bg-white"
-            to="/"
-            >Quick Move
-        </NuxtLink>
+      <NuxtLink
+        class="p-1 lg:bg-black md:bg-white rounded-md hover:bg-white"
+        to="/"
+        >Quick Move
+      </NuxtLink>
 
-        <div class="m-auto lg:block sm:hidden w-2/4 flex">
-            <label class="mr-2" for="">Search Movie</label>
-            <input
-            class="w-3/4 mr-2 pl-2 text-black rounded-md"
-            type="text"
-            name=""
-            id=""
-            />
-            <button class="hover:bg-white rounded-md p-1 bg-black">Search</button>
-        </div>
+      <div class="m-auto lg:block sm:hidden w-2/4 flex">
+        <label class="mr-2" for="">Search Movie</label>
+        <input
+          class="w-3/4 mr-2 pl-2 text-black rounded-md"
+          type="text"
+          name=""
+          id=""
+        />
+        <button class="hover:bg-white rounded-md p-1 bg-black">Search</button>
+      </div>
 
-        <div class="right-0 ml-auto">
-            <ul class="flex space-x-2">
-            <NuxtLink class="p-1 lg:bg-black rounded-md hover:bg-white" to="login"
-                >Login
-            </NuxtLink>
-            <NuxtLink
-                class="p-1 lg:bg-black rounded-md hover:bg-white"
-                to="register"
-                >Register
-            </NuxtLink>
-            <!-- <li>Login</li>
+      <div class="right-0 ml-auto">
+        <ul class="flex space-x-2">
+          <NuxtLink class="p-1 lg:bg-black rounded-md hover:bg-white" to="login"
+            >Login
+          </NuxtLink>
+          <NuxtLink
+            class="p-1 lg:bg-black rounded-md hover:bg-white"
+            to="register"
+            >Register
+          </NuxtLink>
+          <!-- <li>Login</li>
             <li>Register</li> -->
-            </ul>
-        </div>
-        </div>
+        </ul>
+      </div>
     </div>
-    </template>
+  </div>
+</template>
 
-    <script>
-    export default {
-    name: "Hero",
-    };
-    </script>
+<script>
+export default {
+  name: "Hero",
+};
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,115 @@
 <template>
-    <div class="text-black">
-        <h1>Login, Work in progress</h1>
-    </div>
-  </template>
-  
-  <script setup>
-import { ref } from 'vue';
+  <div class="h-full text-center">
+    <div
+      class="m-auto bg-black border-white border text-white blur-0 rounded-3xl bg-op-40 sm:w-3/4 w-2/4 h-3/4"
+    >
+      <div class="mt-2">
+        <h1>Sign In</h1>
+      </div>
+      <!-- <button onclick="addData">Testing Database</button> -->
+      <div class="h-full">
+        <div class="flex justify-center items-center h-4/5 bg-gray-100">
+          <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-lg">
+            <form @submit.prevent="register">
+              <div class="mb-4"></div>
+              <div class="mb-4">
+                <label
+                  for="email"
+                  class="block text-sm font-medium text-gray-700 mb-1"
+                  >Email Address</label
+                >
+                <input
+                  type="email"
+                  id="email"
+                  v-model="email"
+                  class="block w-full px-3 py-2 text-black border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="you@example.com"
+                  required
+                />
+              </div>
 
-  </script>
-  
+              <div class="mb-4">
+                <label
+                  for="password"
+                  class="block text-sm font-medium text-gray-700 mb-1"
+                  >Password</label
+                >
+                <input
+                  type="password"
+                  id="password"
+                  v-model="password"
+                  class="block w-full px-3 py-2 text-black border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="••••••••"
+                  required
+                />
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  class="w-full px-4 py-2 bg-indigo-600 text-white font-medium rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Sign In
+                </button>
+                <h1 class="text-black">Or Create Account</h1>
+                <br>
+                <NuxtLink
+                  class="w-full px-20 py-2 sm:px-10 bg-indigo-600 text-white font-medium rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  to="/register"
+                  >Create Account
+                </NuxtLink>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+const { $account, $databases, $ID } = useNuxtApp();
+
+const loggedInUser = ref(null);
+const email = ref("");
+const password = ref("");
+const name = ref("");
+
+const login = async (email, password) => {
+  await $account.createEmailPasswordSession(email, password);
+  loggedInUser.value = await $account.get();
+};
+
+const register = async () => {
+  await $account.create("lol", email.value, password.value, name.value);
+  login(email.value, password.value);
+};
+
+const logout = async () => {
+  await $account.deleteSession("current");
+  loggedInUser.value = null;
+};
+
+const form = ref({
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+  termsAccepted: false,
+});
+
+const handleSubmit = () => {
+  if (form.value.password !== form.value.confirmPassword) {
+    alert("Passwords don't match.");
+    return;
+  }
+  // Handle form submission
+  alert("Form submitted successfully!");
+};
+</script>
+
+<style scoped>
+/* Add any additional styles here */
+</style>
