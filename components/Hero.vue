08e4724d914732fdf7d927e26267f178f689de73
Lighterchu <benjaminrowe0@gmail.com>
2024-08-16 18:30:36 +1000

    have fixed the lining of the search button and logo

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,23 +1,25 @@
     <template>
-    <div class="header bg-gray-900">
+    <div class="header bg-gray-900 mb-10">
         <div class="nav-container text-red-600 p-2 font-medium flex">
         <!-- Mobile View -->
         <div
-            class="bg-gray-900 top-0 sm:block lg:hidden w-full h-10 left-0 absolute flex justify-center"
+            class="bg-gray-900 top-0 sm:block lg:hidden w-full h-20 left-0 absolute flex justify-center"
         >
             <!-- <NuxtLink class="p-1 lg:bg-black rounded-md hover:bg-white" to="/"
             >Quick Move
             </NuxtLink> -->
-            <div class="flex">
-                <NuxtImg src="/logo.png" sizes="60px sm:50px md:400px" class="rounded-full" />
-                <label class="mr-2 mt-2 ml-5" for="">Search Movie</label>
+            <div class="flex p-1 ">
+                <nuxt-link to="/">
+                    <NuxtImg src="/logo.png" sizes="sm:60px md:200px" class="rounded-full" />
+                </nuxt-link>
+                <label class="mr-2 mt-2 ml-5">Search Movie</label>
             <input
             class="w-1/4 h-2/4 text-black mt-2 rounded-md pl-2 mr-2"
             type="text"
             name=""
             id=""
             />
-            <button class="hover:bg-white rounded-md p-1 bg-black">Search</button>
+            <button class="hover:bg-white rounded-md sm:right-10 right-1 absolute mt-2 mr-1 p-1 bg-black">Search</button>
             </div>
         </div>
         <!-- --- -->
