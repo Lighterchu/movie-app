bb9a8672caec8a5e07b6c473c4881e676ce706dd
Lighterchu <benjaminrowe0@gmail.com>
2024-08-16 17:51:35 +1000

    have added a logo to the moblie version

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,60 +1,64 @@
-<template>
-  <div class="header bg-gray-900">
-    <div class="nav-container text-red-600 p-2 font-medium flex">
-      <div
-        class="bg-gray-900 top-0 sm:block lg:hidden w-full h-10 left-0 absolute flex justify-center"
-      >
+    <template>
+    <div class="header bg-gray-900">
+        <div class="nav-container text-red-600 p-2 font-medium flex">
+        <!-- Mobile View -->
+        <div
+            class="bg-gray-900 top-0 sm:block lg:hidden w-full h-10 left-0 absolute flex justify-center"
+        >
+            <!-- <NuxtLink class="p-1 lg:bg-black rounded-md hover:bg-white" to="/"
+            >Quick Move
+            </NuxtLink> -->
+            <div class="flex">
+                <NuxtImg src="/logo.png" sizes="60px sm:50px md:400px" class="rounded-full" />
+                <label class="mr-2 mt-2 ml-5" for="">Search Movie</label>
+            <input
+            class="w-1/4 h-2/4 text-black mt-2 rounded-md pl-2 mr-2"
+            type="text"
+            name=""
+            id=""
+            />
+            <button class="hover:bg-white rounded-md p-1 bg-black">Search</button>
+            </div>
+        </div>
+        <!-- --- -->
+
         <NuxtLink
-          class="p-1 lg:bg-black  rounded-md hover:bg-white"
-          to="/"
-          >Quick Move
+            class="p-1 lg:bg-black md:bg-white rounded-md hover:bg-white"
+            to="/"
+            >Quick Move
         </NuxtLink>
-        <label class="mr-2 mt-2 ml-5" for="">Search Movie</label>
-        <input
-          class="w-1/4 h-2/4 text-black mt-2 rounded-md pl-2 mr-2"
-          type="text"
-          name=""
-          id=""
-        />
-        <button class="hover:bg-white rounded-md p-1 bg-black">Search</button>
-      </div>
-      <NuxtLink
-        class="p-1 lg:bg-black md:bg-white rounded-md hover:bg-white"
-        to="/"
-        >Quick Move
-      </NuxtLink>
 
-      <div class="m-auto lg:block sm:hidden w-2/4 flex">
-        <label class="mr-2" for="">Search Movie</label>
-        <input
-          class="w-3/4 mr-2 pl-2 text-black rounded-md"
-          type="text"
-          name=""
-          id=""
-        />
-        <button class="hover:bg-white rounded-md p-1 bg-black">Search</button>
-      </div>
+        <div class="m-auto lg:block sm:hidden w-2/4 flex">
+            <label class="mr-2" for="">Search Movie</label>
+            <input
+            class="w-3/4 mr-2 pl-2 text-black rounded-md"
+            type="text"
+            name=""
+            id=""
+            />
+            <button class="hover:bg-white rounded-md p-1 bg-black">Search</button>
+        </div>
 
-      <div class="right-0 ml-auto">
-        <ul class="flex space-x-2">
-          <NuxtLink class="p-1 lg:bg-black rounded-md hover:bg-white" to="login"
-            >Login
-          </NuxtLink>
-          <NuxtLink
-            class="p-1 lg:bg-black rounded-md hover:bg-white"
-            to="register"
-            >Register
-          </NuxtLink>
-          <!-- <li>Login</li>
-          <li>Register</li> -->
-        </ul>
-      </div>
+        <div class="right-0 ml-auto">
+            <ul class="flex space-x-2">
+            <NuxtLink class="p-1 lg:bg-black rounded-md hover:bg-white" to="login"
+                >Login
+            </NuxtLink>
+            <NuxtLink
+                class="p-1 lg:bg-black rounded-md hover:bg-white"
+                to="register"
+                >Register
+            </NuxtLink>
+            <!-- <li>Login</li>
+            <li>Register</li> -->
+            </ul>
+        </div>
+        </div>
     </div>
-  </div>
-</template>
+    </template>
 
-<script>
-export default {
-  name: "Hero",
-};
-</script>
+    <script>
+    export default {
+    name: "Hero",
+    };
+    </script>
