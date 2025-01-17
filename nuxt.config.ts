9223cf512dc66e1ab81558993245ea8a2bd6e3e7
Lighterchu <benjaminrowe0@gmail.com>
2025-01-17 17:05:38 +1100

    fixing meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
     head: {
       title: 'Quick Move - Latest Streaming Movie Info',
       meta: [
-        // General meta tags
         {
           hid: 'description',
           name: 'description',
@@ -15,7 +13,6 @@ export default defineNuxtConfig({
           name: 'keywords',
           content: 'movies, stream, streaming, quickmove',
         },
-        // Open Graph meta tags
         {
           property: 'og:title',
           content: 'Quick Move - Latest Streaming Movie Info',
@@ -26,58 +23,27 @@ export default defineNuxtConfig({
         },
         {
           property: 'og:image',
-          content: '/movie-app-image.png', // Replace with your image URL
+          content: '/movie-app-image.png', // Image in the public folder
         },
         {
           property: 'og:url',
-          content: 'https://yourapp.com', // Replace with your app URL
+          content: 'https://quickmove.netlify.app/', // Replace with your app URL
         },
         {
           property: 'og:type',
           content: 'website',
         },
-        // Twitter Card meta tags
-        {
-          name: 'twitter:card',
-          content: '/image/movie-app-image.png',
-        },
-        {
-          name: 'twitter:title',
-          content: 'Quick Move - Latest Streaming Movie Info',
-        },
-        {
-          name: 'twitter:description',
-          content: 'Get all the latest streaming movies in theaters & online',
-        },
-        {
-          name: 'twitter:image',
-          content: '/image/movie-app-image.png', // Replace with your image URL
-        },
       ]
     }
   },
-
-  
-  devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-
-  runtimeConfig: {
-    public: {
-      TMDB: process.env.NUXT_PUBLIC_TMDB
-    }
-  },
-
   modules: [
-    "@nuxt/image",
+    '@nuxt/image',
   ],
-
-
-  compatibilityDate: "2024-08-15",
 })
